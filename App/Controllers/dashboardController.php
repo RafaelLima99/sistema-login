5c3e67e40521d571cd1c00b882f68a7d51fdd8fa
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-11 17:47:35 -0200

    modificado

---
+++ App/Controllers/dashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class DashboardController extends Action {
+
+	public function index() {
+        session_start();
+        $this->verificaLogin();
+		$this->render('dashboard');
+		 
+    }
+    
+    public function verificaLogin(){
+        
+        if(!$_SESSION['logado']){
+            header('Location: /login?logado=false');
+        }
+    }
+
+}
+
+
+?>
